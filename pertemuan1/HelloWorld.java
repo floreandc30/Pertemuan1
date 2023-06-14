340a7a07793225037cf94e86579b004036351d95
Florean Desvita Cindirahayu <128340713+floreandc30@users.noreply.github.com>
2023-06-14 21:49:48 +0700

    Add files via upload

---
+++ pertemuan1/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.Florean_Desvita_Cindirahayu.pbo.pertemuan1;
+
+public class HelloWorld {
+    public static void main(String[]args) {
+        System.out.println("Hello World");
+    }
+}
